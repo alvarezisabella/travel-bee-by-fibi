4907038b7c70d7c44ccc1edeaec988ae34695806
alvarezisabella <isabellaalvarez003@gmail.com>
2026-03-06 11:19:58 -0800

    added trip header to itinerary page

--- app/itinerary/page.tsx
+++ app/itinerary/page.tsx
@@ -1,5 +1,31 @@
 import EventCardPreview from "./event";
+import TripHeader from "./components/TripHeader";
+import { Trip } from "./types/trips";
 
-export default function itineraryList(){
-    return <EventCardPreview/>
+async function getTrip(): Promise<Trip> {
+  return {
+    id: "1",
+    title: "Hawaii Summer '26",
+    location: "Honolulu, Hawaii",
+    startDate: "Jun 15",
+    endDate: "Jun 22",
+    coverImage: "image placeholder",
+    travelers: [
+      { id: "1", name: "Traveler 1" },
+      { id: "2", name: "Traveler 2" },
+      { id: "3", name: "Traveler 3" },
+      { id: "4", name: "Traveler 4" }
+    ]
+  }
+}
+
+
+export default async function itineraryList(){
+    const trip = await getTrip()
+    return (
+    <main className="bg-gray-100 min-h-screen p-10">
+      <TripHeader trip={trip} />
+      <EventCardPreview/>
+    </main>
+  );
 }
---
+++ app/itinerary/types/trips.ts
@@ -0,0 +1,15 @@
+export interface Traveler {
+  id: string
+  name: string
+  avatar?: string
+}
+
+export interface Trip {
+  id: string
+  title: string
+  location?: string
+  startDate?: string
+  endDate?: string
+  travelers: Traveler[]
+  coverImage: string
+}
