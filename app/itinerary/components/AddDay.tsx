105d51f6b049e4aeeb28d9749b13cc6a5249eff1
alvarezisabella <isabellaalvarez003@gmail.com>
2026-03-12 12:01:01 -0700

    edit add day

--- app/itinerary/components/AddDay.tsx
+++ app/itinerary/components/AddDay.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { Plus } from "lucide-react"
+import { Plus, Trash2 } from "lucide-react"
 
 export default function AddDayButton() {
   const [open, setOpen] = useState(false)
@@ -16,27 +16,34 @@ export default function AddDayButton() {
     setOpen(false)
   }
 
+  const handleDelete = (indexToDelete: number) => {
+    const updated = savedDates.filter((_, i) => i !== indexToDelete)
+    setSavedDates(updated)
+  }
+
   return (
     <div className="mt-6">
 
       {/* Add Day Button */}
-      <button
-        onClick={() => setOpen(true)}
-        className="
-          w-full max-w-6xl mx-auto
-          flex items-center justify-center gap-2
-          bg-gray-100
-          border border-orange-400
-          rounded-xl
-          py-4
-          text-lg font-medium
-          hover:bg-gray-200
-          transition
-        "
-      >
-      <Plus size={20} className="text-orange-500" />
-      Add Day
-      </button>
+      {savedDates.length === 0 && (
+        <button
+          onClick={() => setOpen(true)}
+          className="
+            w-full max-w-6xl mx-auto
+            flex items-center justify-center gap-2
+            bg-gray-100
+            border border-orange-400
+            rounded-xl
+            py-4
+            text-lg font-medium
+            hover:bg-gray-200
+            transition
+          "
+        >
+          <Plus size={20} className="text-orange-500" />
+          Add Day
+        </button>
+      )}
 
       {/* Date Modal */}
       {open && (
@@ -75,9 +82,30 @@ export default function AddDayButton() {
             mx-auto
           "
         >
+        <div className="flex items-center justify-between">
           <h2 className="text-xl font-semibold">
             Day {index + 1}
           </h2>
+
+          {/* CORNER BUTTONS */}
+          <div className="flex gap-3">
+
+            <button
+              onClick={() => setOpen(true)}
+              className="p-2 rounded-md hover:bg-gray-300"
+            >
+              <Plus size={18}/>
+            </button>
+
+            <button
+              onClick={() => handleDelete(index)}
+              className="p-2 rounded-md hover:bg-red-200"
+            >
+              <Trash2 size={18}/>
+            </button>
+
+          </div>
+        </div>
 
           <p className="text-gray-700 mt-1">
             {new Date(d + "T00:00:00").toLocaleDateString("en-US", {
