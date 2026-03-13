78ff409d723d00ccaf778862dc5765f52c43c216
brenda <b.gregann@gmail.com>
2026-03-13 01:05:24 -0700

    update mock data

--- app/itinerary/page.tsx
+++ app/itinerary/page.tsx
@@ -2,6 +2,20 @@ import DayPreview from "./day";
 import TripHeader from "./components/TripHeader";
 import AddDay from "./components/AddDay";
 import { Trip } from "./types/trips";
+import TripList from "./components/TripCard"
+import {Event} from "./event"
+import {Day} from "./day"
+
+
+const SAMPLE_EVENTS:Event[] = [
+    { id: "1", tripid: "1", dayid: "1", title: "Morning Event", description: "detail1.", status: "Confirmed", startTime: "09:00", duration: 30, location: "", travelers: "", type: "Activity" },
+    { id: "2", tripid: "1", dayid: "1", title: "brunch", description: "detail2", status: "Pending", startTime: "11:00", duration: 60, location: "", travelers: "", type: "Food" },
+    { id: "3", tripid: "1", dayid: "1", title: "shopping event", description: "", status: "Confirmed", startTime: "12:30", duration: 90, location: "", travelers: "", type: "Transit" },
+    { id: "4", tripid: "1", dayid: "1", title: "afternoon event", description: "detail3", status: "Confirmed", startTime: "14:00", duration: 120, location: "", travelers: "", type: "Reservation" },
+    { id: "5", tripid: "1", dayid: "1", title: "evening time", description: "", status: "Pending", startTime: "18:00", duration: 60, location: "", travelers: "", type: "Activity" },
+  ]
+
+const MOCK_DAYS:Day[] = [{id: "1", tripid: "1", events: SAMPLE_EVENTS}, {id: "2", tripid: "1", events:[]}]
 
 async function getTrip(): Promise<Trip> {
   return {
@@ -16,7 +30,8 @@ async function getTrip(): Promise<Trip> {
       { id: "2", name: "Traveler 2" },
       { id: "3", name: "Traveler 3" },
       { id: "4", name: "Traveler 4" }
-    ]
+    ],
+    days: MOCK_DAYS
   }
 }
 
@@ -26,8 +41,7 @@ export default async function itineraryList(){
     return (
     <main className="bg-gray-100 min-h-screen p-10">
       <TripHeader trip={trip} />
-      <DayPreview/>
-      <AddDay />
+      <TripList trip={trip}/>
     </main>
   );
 }
